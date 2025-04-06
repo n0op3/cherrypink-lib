58d119c6462970be87e4ee4dae1e3836bee1f21c
ShizukanaOtaku <otakushizukana@gmail.com>
2025-04-06 13:54:56 +0200

    add spdlog and rename the files to sth sensible

--- Include.lua
+++ Include.lua
@@ -2,6 +2,16 @@ project "TinyChernoLib"
     kind "StaticLib"
     language "C++"
     targetdir "%{prj.location}/bin/%{cfg.buildcfg}"
+    defines { "SPDLOG_COMPILED_LIB" }
 
-    files { "src/**", "lib/glfw-3.4/src/**", "lib/glad/src/**" }
-    includedirs { "lib/glfw-3.4/include", "lib/glad/include" }
+    files {
+        "src/**",
+        "lib/glfw-3.4/src/**",
+        "lib/glad/src/**",
+        "lib/spdlog/src/**"
+    }
+    includedirs {
+        "lib/glfw-3.4/include",
+        "lib/glad/include",
+        "lib/spdlog/include"
+    }
--- src/tiny_cherno.cpp
+++ src/tiny_cherno.cpp
@@ -1,9 +1,11 @@
-#include "test_file.hpp"
+#include "tiny_cherno.hpp"
 #include "glad/glad.h"
 
 #include "GLFW/glfw3.h"
+#include "spdlog/spdlog.h"
 
 void tiny_cherno::init() {
+    spdlog::info("Loading the TinyCherno runtime!");
     glfwInit();
     gladLoadGL();
 }
