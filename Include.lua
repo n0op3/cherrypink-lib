9615ceb4a40eb203a1ac92d852343eddbf20ee3f
ShizukanaOtaku <otakushizukana@gmail.com>
2025-04-11 17:21:07 +0200

    adjust the build scripts to build on Windows

--- Include.lua
+++ Include.lua
@@ -4,28 +4,36 @@ libdirs {
 
 links { "glfw3" }
 
+filter "system:windows" -- I have no clue why Visual Studio won't compile without this, frick you, Microsoft
+        buildoptions { "/utf-8" }
+        defines { "_UNICODE", "UNICODE" }
+
 project "GLFW"
     kind "StaticLib"
     language "C"
     local glfwDir = path.join(_SCRIPT_DIR, "lib", "glfw-3.4")
 
+    -- We need to compile GLFW manually, as it is not a Premake project
     prebuildcommands {
-        "{CHDIR} " .. glfwDir .. "; {MKDIR} build; {CHDIR} build; cmake ..; make"
+         "{CHDIR} " .. glfwDir,
+         "cmake -S . -B build -A x64 && cmake --build build --config %{cfg.buildcfg}"
     }
 
 project "TinyChernoLib"
     kind "StaticLib"
     language "C++"
     cppdialect "C++20"
-    staticruntime "on"
     targetdir "%{prj.location}/bin/%{cfg.buildcfg}"
     defines { "SPDLOG_COMPILED_LIB" }
+
+    dependson { "GLFW" }
 
     files {
         "src/**",
         "lib/glad/src/**",
         "lib/spdlog/src/**"
     }
+
     includedirs {
         "lib/glfw-3.4/include",
         "lib/glad/include",
