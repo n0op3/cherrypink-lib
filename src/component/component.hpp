ad2b1674058115cdce1a20f1d08c52185822d1bf
ShizukanaOtaku <otakushizukana@gmail.com>
2025-04-13 21:09:52 +0200

    cleanup the ECS a bit more

--- src/component/component.hpp
+++ src/component/component.hpp
@@ -1,6 +1,8 @@
+//HACK: This whole file is one big brainf**k. Proceed at your own risk.
+
 #pragma once
 
-#include "entity/entity.hpp"
+#include "component/system.hpp"
 #include "util/uuid.hpp"
 #include <memory>
 #include <spdlog/spdlog.h>
@@ -9,62 +11,31 @@
 
 namespace tiny_cherno {
 
-template<typename T>
-class System {
-public:
-    virtual void ProcessComponent(const UUID& entityUuid, T& object) = 0;
-    virtual ~System() = default;
-};
-
-class SystemRegistry {
-public:
-    template<typename T>
-    void RegisterSystem(std::shared_ptr<System<T>> system) {
-        m_systems[std::type_index(typeid(T))] = system;
-    }
-
-    template<typename T>
-    void process(const UUID& entityUuid, T& component) {
-        auto it = m_systems.find(std::type_index(typeid(T)));
-        if (it != m_systems.end()) {
-            auto processor = std::static_pointer_cast<System<T>>(it->second);
-            processor->ProcessComponent(entityUuid, component);
-        }
-    }
-
-private:
-    std::unordered_map<std::type_index, std::shared_ptr<void>> m_systems;
-};
-
-struct ObjectWrapper {
-    virtual ~ObjectWrapper() = default;
+struct ComponentWrapper {
+    virtual ~ComponentWrapper() = default;
     virtual void Process(const UUID& entityUuid, SystemRegistry& registry) = 0;
 };
 
 template<typename T>
-struct TypedObjectWrapper : ObjectWrapper {
+struct TypedObjectWrapper : ComponentWrapper {
     T component;
     TypedObjectWrapper(T object) : component(std::move(object)) {}
     void Process(const UUID& entityUuid, SystemRegistry& registry) override {
-        registry.process(entityUuid, component);
+        registry.ProcessComponent(entityUuid, component);
     }
 };
 
 class ComponentRegistry {
-private:
-    std::unordered_map<std::type_index, std::unordered_map<UUID, std::unique_ptr<ObjectWrapper>>> m_components;
-    SystemRegistry m_systems;
-
 public:
     template<typename T>
-    void AttachComponent(Entity entity, T comp) {
-        m_components[std::type_index(typeid(T))][entity.Uuid] = std::make_unique<TypedObjectWrapper<T>>(std::move(comp));
+    void AttachComponent(const UUID &uuid, T comp) {
+        m_components[std::type_index(typeid(T))][uuid] = std::make_unique<TypedObjectWrapper<T>>(std::move(comp));
     }
 
     template<typename T>
-    std::optional<T*> GetComponent(Entity entity) {
+    std::optional<T*> GetComponent(const UUID &uuid) {
         auto& component_map = m_components[std::type_index(typeid(T))];
-        if (auto it = component_map.find(entity.Uuid); it != component_map.end()) {
+        if (auto it = component_map.find(uuid); it != component_map.end()) {
             TypedObjectWrapper<T>* typed_wrapper = static_cast<TypedObjectWrapper<T>*>(it->second.get());
             if (typed_wrapper) {
                 return &typed_wrapper->component;
@@ -80,6 +51,10 @@ public:
             }
         }
     }
+
+private:
+    std::unordered_map<std::type_index, std::unordered_map<UUID, std::unique_ptr<ComponentWrapper>>> m_components;
+    SystemRegistry m_systems;
 };
 
 
--- src/component/system.hpp
+++ src/component/system.hpp
@@ -1,52 +1,38 @@
 #pragma once
 
-#include <any>
+#include "util/uuid.hpp"
 #include <memory>
 #include <typeindex>
 #include <unordered_map>
-#include "entity/entity.hpp"
 
 namespace tiny_cherno {
 
 template<typename T>
-class ComponentSystem {
-    public:
-        virtual void ProcessComponent(Entity &entity, T& component) = 0;
+
+class System {
+public:
+    virtual void ProcessComponent(const UUID& entityUuid, T& object) = 0;
+    virtual ~System() = default;
 };
 
-class Systems {
-    public:
-        template<typename T>
-            void RegisterSystem(std::shared_ptr<ComponentSystem<T>> system) {
-                m_systems[std::type_index(typeid(T))] = system;
-            }
+class SystemRegistry {
+public:
+    template<typename T>
+    void RegisterSystem(std::shared_ptr<System<T>> system) {
+        m_systems[std::type_index(typeid(T))] = system;
+    }
 
-        template<typename T>
-            bool HasRegisteredSystem() {
-                return HasRegisteredSystem(std::type_index(typeid(T)));
-            }
-
-        bool HasRegisteredSystem(const std::type_index index) const {
-            return m_systems.find(index) != m_systems.end();
+    template<typename T>
+    void ProcessComponent(const UUID& entityUuid, T& component) {
+        auto it = m_systems.find(std::type_index(typeid(T)));
+        if (it != m_systems.end()) {
+            auto processor = std::static_pointer_cast<System<T>>(it->second);
+            processor->ProcessComponent(entityUuid, component);
         }
+    }
 
-        template<typename T>
-            void UnregisterSystem() {
-                m_systems.erase(std::type_index(typeid(T)));
-            }
-
-        template<typename T>
-            bool ProcessComponent(Entity &entity, T &component) {
-                auto it = m_systems.find(std::type_index(component.type()));
-                if (it != m_systems.end()) {
-                    std::static_pointer_cast<ComponentSystem<T>>(it->second)->ProcessComponent(entity, component);
-                    return true;
-                }
-                return false;
-            }
-
-    private:
-        std::unordered_map<std::type_index, std::unique_ptr<ComponentSystem<std::any>>> m_systems;
+private:
+    std::unordered_map<std::type_index, std::shared_ptr<void>> m_systems;
 };
 
 }
