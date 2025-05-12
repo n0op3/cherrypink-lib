954feb4ca7ef37b30c94f5168ee21c001649d1bb
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-12 17:55:20 +0200

    after 6 hours... add assimp

--- Include.lua
+++ Include.lua
@@ -3,10 +3,10 @@ includedirs {
     "lib/glfw-3.4/include",
     "lib/glad/include",
     "lib/spdlog/include",
-    "lib/glm"
+    "lib/glm",
+    "lib/assimp/include",
+    "build/include",
 }
-
-local glfwDir = path.join(_SCRIPT_DIR, "lib", "glfw-3.4")
 
 filter "system:windows" -- I have no clue why Visual Studio won't compile without this, frick you, Microsoft
         buildoptions { "/utf-8" }
@@ -23,25 +23,30 @@ project "CherryPink"
     targetdir "%{prj.location}/bin/%{cfg.buildcfg}"
     defines { "SPDLOG_COMPILED_LIB" }
 
-    -- We need to compile GLFW manually, as it is not a Premake project
+    -- We need to compile these manually, as they are not Premake projects
+    local glfwDir = path.join(_SCRIPT_DIR, "lib", "glfw-3.4")
+    local assimpDir = path.join(_SCRIPT_DIR, "lib", "assimp")
+
     prebuildcommands {
-        "cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -S " .. glfwDir .. " .",
-        "cmake --build . --config %{cfg.buildcfg}",
+        "cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -S " .. glfwDir .. " glfw",
+        "cmake --build glfw --config %{cfg.buildcfg}",
+        "cmake -DBUILD_SHARED_LIBS=OFF -DASSIMP_NO_EXPORT=ON -DASSIMP_INSTALL=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ZLIB=ON -DASSIMP_BUILD_MINIZIP=ON -S " .. assimpDir  .. " assimp",
+        "cmake --build assimp --config %{cfg.buildcfg}",
     }
 
     filter "system:windows"
         prebuildcommands {
-            "{COPYFILE} src/%{cfg.buildcfg}/*glfw* bin/%{cfg.buildcfg}"
+            "{COPYFILE} glfw/src/%{cfg.buildcfg}/*glfw* bin/%{cfg.buildcfg}",
+            "{COPYFILE} assimp/src/%{cfg.buildcfg}/*assimp* bin/%{cfg.buildcfg}",
         }
     filter {}
 
     filter "system:not windows"
         prebuildcommands {
-            "{COPYFILE} src/*glfw3* bin/%{cfg.buildcfg}"
+            "{COPYFILE} glfw/src/*glfw3* bin/%{cfg.buildcfg}",
+            "{COPYFILE} assimp/lib/*assimp* bin/%{cfg.buildcfg}",
         }
     filter {}
-
-    links { "glfw3", "glm" }
 
     files {
         "src/**",
